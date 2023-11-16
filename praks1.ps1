8e11eb1b012679957f34ab76f4d20c75886822ac
unknown <mpihlik@gmail.com>
2023-11-16 08:43:49 +0000

    practice 1

---
+++ praks1.ps1
@@ -0,0 +1,11 @@
+﻿$array1 = @(1, 2, 3)
+$array2 = @(4, 5, 6)
+$array1
+Write-Host
+$array2
+Write-Host
+$array3 = @(0, 0, 0)
+$array3[0] = $array1[0] + $array2[0]
+$array3[1] = $array1[1] + $array2[1]
+$array3[2] = $array1[2] + $array2[2]
+$array3
