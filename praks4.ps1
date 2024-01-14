8a2f5e67db0c47cde20729916561b0cf9a9db5e8
markopihlik <147985797+markopihlik@users.noreply.github.com>
2024-01-15 01:11:53 +0200

    Skript mis tuvastaks suurema numbri ara

---
+++ praks4.ps1
@@ -0,0 +1,11 @@
+﻿$vaartus1 = Read-Host "Enter the first value"
+$vaartus2 = Read-Host "Enter the second value"
+if($vaartus1 -gt $vaartus2)
+    {
+        Write-Host "The higher number is : "$vaartus1
+    }
+else
+    {
+        Write-Host "The higher number is : "$vaartus2
+    }
+#Skript end
