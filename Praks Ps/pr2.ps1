4a4f6619e0c6e0411c276c10d746a6c844bae075
markopihlik <147985797+markopihlik@users.noreply.github.com>
2024-01-15 01:36:39 +0200

    Local kasutajate kustutamine süsteemist

---
+++ Praks Ps/pr2.ps1
@@ -0,0 +1,15 @@
+﻿# Küsime kasutajalt ees- ja perekonnanime
+$firstName = Read-Host -Prompt "Sisesta kasutaja eesnimi"
+$lastName = Read-Host -Prompt "Sisesta kasutaja perekonnanimi"
+
+# Loome kasutajanime
+$userName = ($firstName + $lastName).Replace(" ", "").ToLower()
+
+# Kustutame vastava kasutajanimega lokaalse kasutaja
+try {
+    Remove-LocalUser -Name $userName -ErrorAction Stop
+
+    Write-Host "Kasutaja $userName on edukalt kustutatud."
+} catch {
+    Write-Host "Kasutaja kustutamine ebaõnnestus või kasutajat $userName ei leitud."
+}
